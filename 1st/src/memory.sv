ccbbc430695c19e3c51283019a5d452e459a6799
kappybar <newton7233@outlook.jp>
2021-12-09 21:10:16 +0900

    memory stall test done

--- 1st/src/memory.sv
+++ 1st/src/memory.sv
@@ -115,7 +115,8 @@ module memory
 endmodule
 
 // temporary memory 
-// always cache hit
+// always cache miss
+// stall 0 ~ 16 clock virtually
 module memory_interface
    (input wire clk,
     input wire rstn,
@@ -123,19 +124,42 @@ module memory_interface
     input wire [31:0] data_in,
     input wire write_enable,
     input wire read_enable,
-    output wire [31:0] data_out,
-    output wire ready,
-    output wire miss
+    output logic [31:0] data_out,
+    output logic ready,
+    output logic miss
    );
 
-   ram_block_data dmem(.clk(clk),
-                       .we(write_enable),
-                       .addr(addr[11:2]),
-                       .di(data_in),
-                       .dout(data_out));
+    logic [31:0] temp_data;
+    ram_block_data dmem(.clk(clk),
+                        .we(write_enable),
+                        .addr(addr[11:2]),
+                        .di(data_in),
+                        .dout(temp_data));
 
-   assign ready = 1'b1;
-   assign miss = 1'b0;
+    logic [3:0] cnt;
+    logic before_read_enable;
+    always_ff @(posedge clk) begin
+        if (~rstn) begin
+            cnt <= 4'b0;
+            ready <= 1'b1;
+            miss <= 1'b0;
+            before_read_enable <= 1'b0;
+        end else begin
+            if (read_enable) begin
+                ready <= 1'b0;
+                miss <= 1'b1;
+                before_read_enable <= 1'b1;
+            end else if (before_read_enable) begin
+                data_out <= temp_data;
+                before_read_enable <= 1'b0;
+            end else if (cnt == 4'b00) begin
+                ready <= 1'b1;
+                miss <= 1'b0;
+            end
+            cnt <= cnt + 4'b1;
+        end
+    end
+
 endmodule
 
 `default_nettype wire
